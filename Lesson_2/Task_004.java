f1c8301b89924e4b080f29985d57f57381443362
Julia Ch <juliecha@yandex.ru>
2023-02-15 22:51:26 +0300

    Lesson_2 - upd task_004

--- Lesson_2/Task_004.java
+++ Lesson_2/Task_004.java
@@ -11,13 +11,19 @@ import java.io.File;
 
 public class Task_004 {
     public static void main(String[] args) {
-        File dir = new File("."); // . - обозначение текущей папки
+        File dir = new File("Lesson_2"); // . - обозначение текущей папки
         dir.list();
+        showExtension(dir.list());
     }
 
     private static void showExtension(String[] fileNames){
         for (int i = 0; i < fileNames.length; i++) {
-            
+            int pos = fileNames[i].lastIndexOf('.'); // возвращает позицию последней точки
+            if (pos == -1){
+                System.out.println();
+                continue; // команда означает, что надо перейти к следующему витку цикла
+            }
+            else System.out.println(fileNames[i].substring(pos+1));
         }
     }
 }
