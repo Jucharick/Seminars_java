1b74c8d681faed9f79f2609f4d2f2449f5509225
Julia Ch <juliecha@yandex.ru>
2023-02-15 21:51:02 +0300

    Lesson_2 - uod task_002

--- Lesson_2/Task_002.java
+++ Lesson_2/Task_002.java
@@ -27,7 +27,7 @@ public class Task_002 {
     }
     private static boolean isPalindrome2(String str) {
         boolean isPalindrome = true;
-        for (int i = 0; i < str.length(); i++) {
+        for (int i = 0; i < str.length()/2; i++) {
             if (str.charAt(i) != str.charAt(str.length() - 1 - i)) {
                 isPalindrome = false;
             }
