49386b0ad8694cf33fe58ce1c628abbb10df1783
Julia Ch <juliecha@yandex.ru>
2023-03-06 20:28:56 +0300

    Add Lesson_7 - OOP

---
+++ Lesson_7/Units/MagClass.java
@@ -0,0 +1,10 @@
+package Lesson_7.Units;
+
+public class MagClass extends BaseHero{
+
+    public MagClass(String name, int hp) {
+        super(name, hp);
+        
+    }
+    
+}
---
+++ Lesson_7/Units/Peasant.java
@@ -0,0 +1,1 @@
+package Lesson_7.Units;
